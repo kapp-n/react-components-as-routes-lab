12d45f0a66103a45121da8d3630a8259ddb31000
Mollie Dickenson <molliedickenson@gmail.com>
2021-04-30 01:42:21 -0400

    commit tree f9a4cb124454817217b0b79423075bd8fcbb6060

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+          {movies.map(movie =>
+          <div>
+            <ul>
+              <li>{movie.title}</li>
+              <li>{movie.time}</li>
+              <li>{movie.genres}</li> 
+            </ul>
+          </div>)}
     </div>
   );
 };
